18a1a54d6b2996823cc8ab36dcbdf4fc392df27d
thoulee <thou_lee@outlook.com>
2025-02-26 23:35:17 +0800

    refactor: 修改 genRandomStr.js 文件以生成小写字母随机字符串

--- genRandomStr.js
+++ genRandomStr.js
@@ -2,8 +2,10 @@ const length = 3;
 let randomStr = '';
 
 for (let i = 0; i < length; i++) {
-    const randomCharCode = Math.floor(Math.random() * 26) + 65;
+    // 97 -> 'a' 122 -> 'z'
+    // 65 -> 'A' 90 -> 'Z'
+    const randomCharCode = Math.floor(Math.random() * 26) + 97;
     randomStr += String.fromCharCode(randomCharCode);
 }
-randomStr = randomStr.toLowerCase();
+// randomStr = randomStr.toLowerCase();
 console.log(randomStr);
