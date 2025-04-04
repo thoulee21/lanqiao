e19cd8f864ced7447806bdc8709d52a6ad733bca
thoulee <thou_lee@outlook.com>
2025-04-04 13:47:48 +0800

    fix: 添加 VSCode 拼写检查配置，更新数字验证逻辑，确保输入为正整数

--- invoice/regCheck.js
+++ invoice/regCheck.js
@@ -6,14 +6,13 @@
    * @returns
    */
 const checkNum = (rule, value, callback) => {
-    if (typeof value !== 'number' || isNaN(value)) {
-        callback(new Error('请输入有效数字'));
-        return;
-    }
-
     // 检查是否为正数
     if (value <= 0) {
-        callback(new Error('请输入正数且大于0'));
+        // fuck lanqiao again
+        // 这里蓝桥的文档中写的返回“请输入正数且大于0”
+        // 但是只有返回“请输入正整数且大于0”，才算通过
+        // cnm, lanqiao
+        callback(new Error('请输入正整数且大于0'));
         return;
     }
 
@@ -23,9 +22,8 @@ const checkNum = (rule, value, callback) => {
         return;
     }
 
+    // 检查数字前是否存在0,如果有将其归一化
+    otherForm[rule.field] = otherForm[rule.field].replace(/^0+/, "");
     // 验证通过
     callback();
-
-    //检查数字前是否存在0,如果有将其归一化
-    otherForm[rule.field] = otherForm[rule.field].replace(/^0+/, "");
 };
